3255d8bca2e4d231db18fefae6d4a4aacc733d2a
Merithril <jana.hofmann07@gmail.com>
2026-02-04 14:19:28 +0100

    prototype for the password generator :)

---
+++ generator.py
@@ -0,0 +1,39 @@
+"""
+This module allows the user to either set a password for Tkinter by themselves or generate one using a password generator.
+
+"""
+
+__author__ = "8033684, Hofmann"
+
+import string
+import random
+
+def generate_password(length: int, lower_case: bool, upper_case: bool, digits: bool) -> str :
+
+    chars = ""
+    if lower_case:
+        chars += "abcdefghijklmnopqrstuvwyz"
+
+    if upper_case:
+        chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    if digits:
+        chars += "0123456789"
+
+    if not chars:
+        return f"Error: please choose at least one character"
+
+    if length <= 0:
+        return f"Error: length must be at least one"
+
+    password = "".join(random.choice(chars) for _ in range(length))
+    return password
+
+
+
+
+
+
+
+if __name__ == "__main__"
+    print(generate_password(1, False, False, False)
