49b3a5cd67cf5d63c32f506e3444f06a9f1a8057
Merithril <jana.hofmann07@gmail.com>
2026-02-07 04:13:11 +0100

    Fertig Style check + docstrings gemacht.

--- caesear_cypher_2.py
+++ caesear_cypher_2.py
@@ -1,16 +1,18 @@
 """
-....
+This module implements the ceasar cipher encryption function
+to shift letters by a fixed number to generate a encryption.
 """
 
 __author__ = "8033684, Hofmann"
 
-import random
-import string
-
-from ai import caesar_encrypt
+from encrypted_gui_2 import caesar_encrypt
 
 
 def encrypt(text: str, shift: int = 5) -> str:
+    """
+    Encrypts text using the caesar cipher with a specific shift value (5).
+    idea for structure from ai.
+    """
     results = ""
     for char in text:
         if char.isupper():
@@ -21,5 +23,11 @@ def encrypt(text: str, shift: int = 5) -> str:
             results += char
         return results
 
-def  decrypt_function(text: str, shift: int = 5) -> str:
+
+def decrypt_function(text: str, shift: int = 5) -> str:
+    """
+    Decrypts a text using caesar cipher with a negative shift value.
+
+    """
+
     return caesar_encrypt(text, -shift)
