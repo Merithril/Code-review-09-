00dd08dfd4ebf40d0c904586d5ffb69378bf613f
Merithril <jana.hofmann07@gmail.com>
2026-02-07 03:53:58 +0100

    Fertig Style check + docstrings gemacht. Sebastian Kommentar

--- main_1.py
+++ main_1.py
@@ -1,12 +1,20 @@
 """
-use this as main entry point
+
+This module functions as the main entry point of the program.
+written with help from Sebastian Keller.
 """
 
 import gui_pw_gen_1
 
+
 def main():
+    """
+
+    Launches the password generator GUI application.
+    """
 
     gui_pw_gen_1.main()
+
 
 if __name__ == "__main__":
     main()
