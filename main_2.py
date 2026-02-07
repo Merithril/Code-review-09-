258bb891eaa73e92fa7e321414490a4281b43e74
Merithril <jana.hofmann07@gmail.com>
2026-02-07 03:54:57 +0100

    Fertig Style check + docstrings gemacht.

--- main_2.py
+++ main_2.py
@@ -1,12 +1,17 @@
 """
-use this as main entry point
+This module is the main entrypoint for the text encryption program.
 """
 
 import encrypted_gui_2
 
+
 def main():
+    """
+    Launches the encryption GUI application.
+    """
 
     encrypted_gui_2.main()
+
 
 if __name__ == "__main__":
     main()
